8b2b8f7bbed9d083e952435a92686af0f8652aec
gregjewi <gregjewi@umich.edu>
2024-06-29 14:36:04 -0400

    new unit tests

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -9,6 +9,15 @@ def test_load_network():
     assert "inp" == network[-3:]
     network = pystorms.networks.load_network("gamma")
     assert "inp" == network[-3:]
+    network = pystorms.networks.load_network("gamma_1yr_normal_storages")
+    assert "inp" == network[-3:]
+    network = pystorms.networks.load_network("gamma_1yr_volume_halved")
+    assert "inp" == network[-3:]
+    network = pystorms.networks.load_network("gamma_volume_halved")
+    assert "inp" == network[-3:]
+    # I actually don't think this is used at all. Testing it anyway.
+    network = pystorms.networks.load_network("new_gamma_1yr_volume_halved")
+    assert "inp" == network[-3:]
     network = pystorms.networks.load_network("epsilon")
     assert "inp" == network[-3:]
     network = pystorms.networks.load_network("theta")
--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -76,6 +76,63 @@ def test_gamma():
     assert env.performance() > 10 ** 5
 
 
+def test_gamma_volume_halved():
+    """
+    Tests for Gamma Scenario, with all basins volume halved
+    """
+    # Initalize your environment
+    env = pystorms.scenarios.gamma_volume_halved()
+    done = False
+    steps = 0
+    while not done:
+        state = env.state()
+        # Check if performance measure is raising error
+        if steps < 1:
+            with pytest.raises(ValueError):
+                env.performance()
+        done = env.step()
+        steps += 1
+        # Check for length of state being returned
+        assert len(state) == 11
+        # Check if data log is working
+        assert len(env.data_log["performance_measure"]) == steps
+
+    # All valves closed - remanent water in basins
+    env = pystorms.scenarios.gamma_volume_halved()
+    done = False
+    while not done:
+        done = env.step(np.zeros(11))
+    assert env.performance() > 10 ** 5
+
+
+def test_gamma_1yr_volume_halved():
+    """
+    Tests for Gamma Scenario, with all basins volume halved over 1 year of rainfall simulation
+
+    only run sim once bc it takes a long time
+    """
+    import datetime
+    # Initalize your environment
+    env = pystorms.scenarios.gamma_1yr_volume_halved()
+    done = False
+    steps = 0
+    while not done:
+        state = env.state()
+        # Check if performance measure is raising error
+        if steps < 1:
+            with pytest.raises(ValueError):
+                env.performance()
+        done = env.step()
+        steps += 1
+        # Check for length of state being returned
+        assert len(state) == 11
+        # Check if data log is working
+        assert len(env.data_log["performance_measure"]) == steps
+    assert env.performance() > 10 ** 5
+    assert env.data_log['simulation_time'][0] == datetime.datetime(2021, 1, 1, 0, 0, 1)
+    assert env.data_log['simulation_time'][-1] == datetime.datetime(2021, 12, 31, 0, 0)
+
+
 def test_theta():
     """
     Tests for Theta Scenario
@@ -118,3 +175,12 @@ def test_epsilon():
         assert len(state) == 13
         # Check if data log is working
         assert len(env.data_log["performance_measure"]) == steps
+
+if __name__ == "__main__":
+    test_alpha()
+    test_beta()
+    test_gamma()
+    test_gamma_volume_halved()
+    test_gamma_1yr_volume_halved()
+    test_theta()
+    test_epsilon()
